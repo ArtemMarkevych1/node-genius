c1a66bf3bb99944775ddb4365a7674ba259ed219
amarkevych <aw.markevich@gmail.com>
2023-11-28 17:03:45 +0200

    Add userSchema. Update .gitignore

---
+++ config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const { URI } = require("./secret");
+
+mongoose
+  .connect(URI)
+  .then(() => console.log("Connected to DB"))
+  .catch((error) => console.log(error));
+
+
+  
